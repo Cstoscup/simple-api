4f5a9393f04960756b8c30c6f9113396cff50a9c
Callie Stoscup <calliestoscup@gmail.com>
2024-07-01 18:01:21 -0400

    Try again with localhost

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   });
 
   useEffect(() => {
-    axios.get("http://api:3000/todos").then((response) => {
+    axios.get("http://localhost:3000/todos").then((response) => {
       setTodos(response.data);
     });
   }, []);
@@ -39,7 +39,7 @@ function App() {
   };
 
   const addTodo = () => {
-    axios.post("http://api:3000/todos", newTodo).then((response) => {
+    axios.post("http://localhost:3000/todos", newTodo).then((response) => {
       setTodos(response.data);
     });
   };
